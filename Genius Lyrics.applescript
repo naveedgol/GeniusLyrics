7065c0b2e9b2dbd889677136454bf18c9d1a9538
Naveed Golafshani <ngolafshani@live.ca>
2017-03-05 20:56:53 -0500

    finished lyric exporter, added python script call to applescript

--- Genius Lyrics.applescript
+++ Genius Lyrics.applescript
@@ -3,9 +3,13 @@ tell application "iTunes"
 		display dialog "No track selected" buttons {"Ok"} with icon caution
 		error number -128
 	end if
+	set songName to name of selection
+	set artistName to name of selection
 end tell
 
-set theFile to "/Library/iTunes/Scripts/lyrics.txt"
+set py to "python main.py " & songName & " " & artistName
+do shell script py
+set theFile to "lyrics.txt"
 set fileContents to read theFile
 display dialog fileContents
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 from bs4 import BeautifulSoup
 
 #extract custom genius api token from txt file, obtained from https://genius.com/api-clients
@@ -6,18 +7,20 @@ def extract_token():
     token = open("API_token.txt", "r")
     return token.read()
 
-# def txt_export(lyrics):
+def txt_export(lyrics):
+    lyrics_file = open("lyrics.txt", "w")
+    lyrics_file.write(lyrics)
 
 #Genius api lacks getting lyrics, manually scrape results
 def lyric_scraper(url):
     source = requests.get(url).text
     source_soup = BeautifulSoup(source, "html.parser")
     lyrics = source_soup.find("lyrics")
-    print(lyrics.get_text().strip())
-    # txt_export(lyrics.get_text().strip())
+    # print(lyrics.get_text().strip())
+    return lyrics.get_text().strip()
 
 #Search Genius for a song with corresponding artist
-def search(song, artist):
+def search(artist, song):
     headers = {"Authorization": "Bearer " + extract_token()}
     search_url = 'http://api.genius.com/search'
     search_data = {'q': song + " " + artist}
@@ -25,10 +28,14 @@ def search(song, artist):
 
     for match in search_results["response"]["hits"]:
         if artist == match["result"]["primary_artist"]["name"]:
-            print(match["result"]["url"])
-            lyric_scraper(match["result"]["url"])
-            return
+            # print(match["result"]["url"])
+            return match["result"]["url"]
 
     #indicate a fail to fetch
 
-search("Antidote", "Travis Scott")
+def export_lyrics(artist, song):
+    song_url = search(artist, song)
+    song_lyrics = lyric_scraper(song_url)
+    txt_export(song_lyrics)
+
+export_lyrics("Drake", "Still Here")
